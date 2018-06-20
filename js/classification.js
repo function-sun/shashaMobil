af1bc9965d73b3bd8628f9dbadea2ef26cdf9e8f
sun <330670679@qq.com>
2018-06-20 21:44:05 +0800

    添加了分类一级页

---
+++ js/classification.js
@@ -0,0 +1,12 @@
+//商品分类与所有品牌切换
+//$(function(){
+//	$("#shangpinfenlei").click(function(){
+//		$(".mainClass").css("display","block");
+//		$(".mainClassT").css("display","none");
+//		$(".paixu").css("display","none");
+//	})
+//	$("#allpinpai").click(function(){
+//		$(".mainClassT").css("display","block");
+//		$(".mainClass").css("display","none");
+//	})
+//})
